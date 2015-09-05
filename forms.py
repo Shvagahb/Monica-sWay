123b69efef6b493af36f89e40e392e9642a212e3
Jesse Lurie <jesse@cuttlesoft.com>
2015-09-05 16:08:21 -0400

    rm forms

--- forms.py
+++
@@ -1,7 +0,0 @@
-from flask_wtf import Form
-from wtforms import StringField
-from wtforms.validators import DataRequired
-
-class SearchForm(Form):
-	first_name = StringField('first_name')
-	last_name = StringField('last_name')
