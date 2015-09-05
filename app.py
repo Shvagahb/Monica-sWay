942b8f76f686d3d53f8a20d759b3ecbf0c7fdc58
Jesse Lurie <jesse@cuttlesoft.com>
2015-09-05 14:26:46 -0400

    binded search results

--- app.py
+++ app.py
@@ -1,15 +1,25 @@
-import couchdb
-from flask import Flask, jsonify, render_template
+from flask import Flask, jsonify, render_template, request, url_for
+from pymongo import *
 
 app = Flask(__name__)
+cli = MongoClient()
+db = cli.finra
 
-@app.route("/")
+@app.route("/",methods=['POST','GET'])
 def index():
 	return render_template("index.html")
 
-@app.route("/search")
+@app.route("/search",methods=['POST','GET'])
 def search_advisors():
-	return render_template("search_results.html")
+	res = []
+	if request.method == 'POST':
+		firstname = str(request.form['firstname'])
+		lastname = str(request.form['lastname'])
+		print firstname, lastname
+		cursor = db.dataset.find({"Info.@firstNm":firstname,"Info.@lastNm":lastname})
+		for i in cursor:
+			res.append(i)
+	return render_template("search_results.html",res=res)
 
 @app.route("/advisor/")
 def view_advisor_detail():
--- finra.py
+++ finra.py
@@ -1,11 +1,19 @@
 import xmltodict, json
 import couchdb
+from pymongo import *
+
+mongo = MongoClient()
+mongodb = mongo.finra
 
 cli = couchdb.Server()
 db = cli['finra']
+#save the xml to couch
+#retrun id of saved xml
+#loop through and save to mongo
 docs = db['3c381ae20cb8003b2ea552696a0009ae']
 for i in docs['finra']['IAPDIndividualReport']['Indvls']['Indvl']:
-	print i
+	#SAVE I AS A INDIVIDUAL
+	mongodb.dataset.insert_one(i)
 
 # for doc in docs.finra:
 # 	for do in doc.IAPDIndividualReport:
