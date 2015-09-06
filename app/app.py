58d349a03c1319f3b868b1f0d98ba72b3944fd79
Jesse Lurie <jesse@cuttlesoft.com>
2015-09-06 12:56:53 -0400

    some stuff

--- app/app.py
+++ app/app.py
@@ -46,6 +46,7 @@ def index():
 @app.route("/search",methods=['POST','GET'])
 def search_advisors():
 	companies = []
+	co = request.form['company']
 	if request.method == 'POST':
 		cursor = db.dataset.find({"CrntEmps.CrntEmp.@orgNm":str(request.form['company'])})
 		for i in cursor:
@@ -53,7 +54,7 @@ def search_advisors():
 		#cursor = db.dataset.find({"Info.@firstNm":firstname,"Info.@lastNm":lastname})
 		# for i in cursor:
 		# 	res.append(i)
-	return render_template("search_results.html",res=companies)
+	return render_template("search_results.html",res=companies,co = co)
 
 @app.route("/advisor/<objectid:id>",methods=['POST','GET'])
 def view_advisor_detail(id):
@@ -62,19 +63,66 @@ def view_advisor_detail(id):
 	passed_examinations = []
 	other_name = []
 	res = []
+	has_civil_judc = None
+	has_cust_comp = None
+	has_bankrupt = None
+	has_reg_action = None
+	has_judgement = None
+	has_investigation = None
+	has_termination = None
+	has_criminal = None
+	has_bond = None
+	has_records = None
 	cursor = db.dataset.find({"_id":id})
+	co = ''
+	st = ''
 	for i in cursor:
+		co = i["CrntEmps"]["CrntEmp"]["@orgNm"]
+		st = i["CrntEmps"]["CrntEmp"]["@state"]
 		if i["OthrNms"] != None:
 			other_name.append(i["OthrNms"])
-		# if i["DPRs"] != None:
-		# 	drps.append(i["DPRs"])
-		
+		if i["DRPs"] != None:
+			if i["DRPs"]["DRP"]["@hasCivilJudc"] == "Y":
+				has_records=True
+				has_civil_judc = True
+			if i["DRPs"]["DRP"]["@hasCustComp"] == "Y":
+				has_records=True
+				has_cust_comp = True
+			if i["DRPs"]["DRP"]["@hasBankrupt"] == "Y":
+				has_records=True
+				has_bankrupt = True
+			if i["DRPs"]["DRP"]["@hasRegAction"] == "Y":
+				has_records=True
+				has_reg_action = True
+			if i["DRPs"]["DRP"]["@hasJudgment"] == "Y":
+				has_records=True
+				has_judgement = True
+			if i["DRPs"]["DRP"]["@hasInvstgn"] == "Y":
+				has_records=True
+				has_investigation = True
+			if i["DRPs"]["DRP"]["@hasTermination"] == "Y":
+				has_records=True
+				has_termination = True
+			if i["DRPs"]["DRP"]["@hasCriminal"] == "Y":
+				has_records=True
+				has_criminal = True
+			if i["DRPs"]["DRP"]["@hasBond"] == "Y":
+				has_records=True
+				has_bond = True
+		else:
+			has_records = None
+
+
+		#{u'DRP': {u'@hasCivilJudc': u'N', u'@hasCustComp': u'Y', u'@hasBankrupt': u'N', u'@hasRegAction': u'N', u'@hasJudgment': u'N', u'@hasInvstgn': u'N', u'@hasTermination': u'N', u'@hasCriminal': u'N', u'@hasBond': u'N'}}
+		# if i["DRPs"] != None:
+		# 	drps.append(i["DRPs"])
+
 		res.append(i)
 
 	#get current company
 	#get passed examinations
 	#get DRP info
-	return render_template("advisor_info.html",result=res)
+	return render_template("advisor_info.html",result=res,has_civil_judc=has_civil_judc,has_cust_comp=has_cust_comp,has_bankrupt=has_bankrupt,has_reg_action=has_reg_action,has_judgement=has_judgement,has_investigation=has_investigation,has_termination=has_termination,has_criminal=has_criminal,has_bond=has_bond,has_records=has_records, co=co,st=st)
 
 @app.route('/autocomplete',methods=['GET'])
 def autocomplete():
