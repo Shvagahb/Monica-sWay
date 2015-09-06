af64d433998333c0f657ce40acb7ea8dcfe89bdc
Jesse Lurie <jesse@cuttlesoft.com>
2015-09-06 00:27:29 -0400

    fixed stuff

--- app/app.py
+++ app/app.py
@@ -1,3 +1,4 @@
+import unicodedata
 from flask import Flask, jsonify, render_template, request, url_for
 from pymongo import *
 from werkzeug.routing import BaseConverter, ValidationError
@@ -19,50 +20,65 @@ app = Flask(__name__)
 app.url_map.converters['objectid'] = ObjectIDConverter
 cli = MongoClient()
 db = cli.finra
-
-
-@app.route("/",methods=['POST','GET'])
-def index():
-	#search_form = SearchForm()
-	co_names = []
-	states = ["AL", "AK", "AZ", "AR", "CA", "CO", "CT", "DC", "DE", "FL", "GA", 
+cursor = db.dataset.find()
+co_names = []
+states = ["AL", "AK", "AZ", "AR", "CA", "CO", "CT", "DC", "DE", "FL", "GA", 
           "HI", "ID", "IL", "IN", "IA", "KS", "KY", "LA", "ME", "MD", 
           "MA", "MI", "MN", "MS", "MO", "MT", "NE", "NV", "NH", "NJ", 
           "NM", "NY", "NC", "ND", "OH", "OK", "OR", "PA", "RI", "SC", 
           "SD", "TN", "TX", "UT", "VT", "VA", "WA", "WV", "WI", "WY"]
-	cursor = db.dataset.find()
-	for i in cursor:
-		if type(i["CrntEmps"]["CrntEmp"]) == type(co_names):
-			co_names.append(i["CrntEmps"]["CrntEmp"][0]["@orgNm"])
-		else:
-			co_names.append(i["CrntEmps"]["CrntEmp"]["@orgNm"])
-	# co_names_unique = []
-	# co_names_unique = [i for i in co_names if i not in co_names_unique]
-	# print co_names_unique
+
+for i in cursor:
+	if type(i["CrntEmps"]["CrntEmp"]) == type(co_names):
+		co_names.append(i["CrntEmps"]["CrntEmp"][0]["@orgNm"])
+	else:
+		co_names.append(i["CrntEmps"]["CrntEmp"]["@orgNm"])
 	co_names = set(co_names)
 	co_names = list(co_names)
-	co_names.sort()
+
+co_names = [x.encode('UTF8') for x in co_names]
+
+@app.route("/",methods=['POST','GET'])
+def index():
+	#search_form = SearchForm()
 	return render_template("index.html",companies=co_names,states=states) #form=search_form)
 
 @app.route("/search",methods=['POST','GET'])
 def search_advisors():
-	res = []
-	if request.method == 'POST' and request.form['firstname'] != '' and request.form['lastname'] !='':
-		firstname = str(request.form['firstname']).upper()
-		lastname = str(request.form['lastname']).upper()
-		cursor = db.dataset.find({"Info.@firstNm":firstname,"Info.@lastNm":lastname})
+	companies = []
+	if request.method == 'POST':
+		cursor = db.dataset.find({"CrntEmps.CrntEmp.@orgNm":str(request.form['company'])})
 		for i in cursor:
-			res.append(i)
-	res = set(res)
-	res = list(res)
-	return render_template("search_results.html",res=res)
+			companies.append(i)
+		#cursor = db.dataset.find({"Info.@firstNm":firstname,"Info.@lastNm":lastname})
+		# for i in cursor:
+		# 	res.append(i)
+	return render_template("search_results.html",res=companies)
 
 @app.route("/advisor/<objectid:id>",methods=['POST','GET'])
 def view_advisor_detail(id):
-	cursor = db.dataset.find({"CrntEmps.CrntEmp.@orgNm"})
+	drps = []
+	prevous_employment = []
+	passed_examinations = []
+	other_name = []
+	res = []
+	cursor = db.dataset.find({"_id":id})
 	for i in cursor:
-		print i
-	return render_template("advisor_info.html")
+		if i["OthrNms"] != None:
+			other_name.append(i["OthrNms"])
+		# if i["DPRs"] != None:
+		# 	drps.append(i["DPRs"])
+		
+		res.append(i)
+
+	#get current company
+	#get passed examinations
+	#get DRP info
+	return render_template("advisor_info.html",result=res)
+
+@app.route('/autocomplete',methods=['GET'])
+def autocomplete():
+	return jsonify(json_list=co_names)
 
 
 if __name__ == "__main__":
--- app/finra.py
+++ app/finra.py
@@ -2,18 +2,22 @@ import xmltodict, json
 import couchdb
 from pymongo import *
 
-mongo = MongoClient()
+mongo = MongoClient("45.55.228.139:27017")
 mongodb = mongo.finra
 
-cli = couchdb.Server()
-db = cli['finra']
+data = open('data.xml')
+data = xmltodict.parse(data)
+for individual in data['IAPDIndividualReport']['Indvls']['Indvl']:
+	mongodb.dataset.insert_one(individual)
+# cli = couchdb.Server()
+# db = cli['finra']
 #save the xml to couch
 #retrun id of saved xml
 #loop through and save to mongo
-docs = db['3c381ae20cb8003b2ea552696a0009ae']
-for i in docs['finra']['IAPDIndividualReport']['Indvls']['Indvl']:
-	#SAVE I AS A INDIVIDUAL
-	mongodb.dataset.insert_one(i)
+#docs = db['3c381ae20cb8003b2ea552696a0009ae']
+# for i in docs['finra']['IAPDIndividualReport']['Indvls']['Indvl']:
+# 	#SAVE I AS A INDIVIDUAL
+# 	mongodb.dataset.insert_one(i)
 
 # for doc in docs.finra:
 # 	for do in doc.IAPDIndividualReport:
